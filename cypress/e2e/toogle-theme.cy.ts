df91a3b4e46b7492568f8a5e65faacf5f93efe44
bivarz <paulo.bivar@gmail.com>
2024-05-27 16:16:19 -0300

    feat: finishing project

--- cypress/e2e/toogle-theme.cy.ts
+++ cypress/e2e/toogle-theme.cy.ts
@@ -6,29 +6,28 @@ describe("ThemeToggle Component", () => {
   it("should toggle the theme between light and dark", () => {
     cy.get("html").should("have.class", "light");
 
-    // Verificar se o ícone do botão é o ícone da lua (indicando modo claro)
+
     cy.get('[data-cy="theme-toggle-button"]').within(() => {
       cy.get('[data-cy="moon-icon"]').should("exist");
     });
 
-    // Clicar no botão para alternar o tema para escuro
     cy.get('[data-cy="theme-toggle-button"]').click();
 
-    // Verificar se a classe do tema mudou para escuro
+
     cy.get("html").should("have.class", "dark");
 
-    // Verificar se o ícone do botão é o ícone do sol (indicando modo escuro)
+
     cy.get('[data-cy="theme-toggle-button"]').within(() => {
       cy.get('[data-cy="sun-icon"]').should("exist");
     });
 
-    // Clicar novamente no botão para alternar o tema de volta para claro
+
     cy.get('[data-cy="theme-toggle-button"]').click();
 
-    // Verificar se a classe do tema mudou de volta para claro
+
     cy.get("html").should("have.class", "light");
 
-    // Verificar se o ícone do botão voltou a ser o ícone da lua (indicando modo claro)
+ 
     cy.get('[data-cy="theme-toggle-button"]').within(() => {
       cy.get('[data-cy="moon-icon"]').should("exist");
     });
