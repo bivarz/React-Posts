98e2acafea548163bb6af99214cc660c3d225755
bivarz <paulo.bivar@gmail.com>
2024-05-27 16:23:28 -0300

    feat: finishing project

--- cypress/e2e/toogle-theme.cy.ts
+++ cypress/e2e/toogle-theme.cy.ts
@@ -6,28 +6,22 @@ describe("ThemeToggle Component", () => {
   it("should toggle the theme between light and dark", () => {
     cy.get("html").should("have.class", "light");
 
-
     cy.get('[data-cy="theme-toggle-button"]').within(() => {
       cy.get('[data-cy="moon-icon"]').should("exist");
     });
 
     cy.get('[data-cy="theme-toggle-button"]').click();
 
-
     cy.get("html").should("have.class", "dark");
-
 
     cy.get('[data-cy="theme-toggle-button"]').within(() => {
       cy.get('[data-cy="sun-icon"]').should("exist");
     });
 
-
     cy.get('[data-cy="theme-toggle-button"]').click();
-
 
     cy.get("html").should("have.class", "light");
 
- 
     cy.get('[data-cy="theme-toggle-button"]').within(() => {
       cy.get('[data-cy="moon-icon"]').should("exist");
     });
